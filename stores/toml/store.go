cf3d959e2fe8dcb783e8a122927af9320cf31bbf
Matthieu Dartiguenave <matthieu.dartiguenave@gmail.com>
2020-07-26 20:00:14 +0100

    Issues with ordering

--- stores/toml/store.go
+++ stores/toml/store.go
@@ -1,5 +1,89 @@
 package toml
 
+import (
+	"fmt"
+
+	"github.com/pelletier/go-toml"
+	"go.mozilla.org/sops/v3"
+	"go.mozilla.org/sops/v3/stores"
+)
+
 // Store handles storage of TOML data
 type Store struct {
+}
+
+// LoadEncryptedFile loads an encrypted secrets file onto a sops.Tree object
+func (store *Store) LoadEncryptedFile(in []byte) (sops.Tree, error) {
+	return sops.Tree{}, nil
+}
+
+func tomlTreeValueToTreeValue(tree interface{}) interface{} {
+
+	switch node := tree.(type) {
+	case []*toml.Tree:
+		return []interface{}{}
+	case *toml.Tree:
+		return tomlTreeToTreeBranch(node)
+
+	default:
+		return node
+	}
+}
+
+func tomlTreeToTreeBranch(tree *toml.Tree) sops.TreeBranch {
+	var root sops.TreeBranch
+
+	fmt.Printf("%#v", tree)
+
+	for _, key := range tree.Keys() {
+		item := sops.TreeItem{
+			Key:   key,
+			Value: tomlTreeValueToTreeValue(tree.Get(key)),
+		}
+
+		root = append(root, item)
+	}
+	return root
+}
+
+// LoadPlainFile loads plaintext toml file bytes onto a sops.TreeBranches object
+func (store *Store) LoadPlainFile(in []byte) (sops.TreeBranches, error) {
+	tree, err := toml.LoadBytes(in)
+
+	if err != nil {
+		return nil, fmt.Errorf("Error unmarshaling input TOML: %s", err)
+	}
+
+	branches := sops.TreeBranches{tomlTreeToTreeBranch(tree)}
+
+	fmt.Printf("%#v\n", branches)
+
+	return branches, nil
+}
+
+// EmitEncryptedFile returns the encrypted bytes of the toml file corresponding to a
+// sops.Tree runtime object
+func (store *Store) EmitEncryptedFile(in sops.Tree) ([]byte, error) {
+	return nil, nil
+}
+
+// EmitPlainFile returns the plaintext bytes of the toml file corresponding to a
+// sops.TreeBranches runtime object
+func (store *Store) EmitPlainFile(in sops.TreeBranches) ([]byte, error) {
+	return nil, nil
+}
+
+// EmitValue returns bytes corresponding to a single encoded value
+// in a generic interface{} object
+func (store *Store) EmitValue(v interface{}) ([]byte, error) {
+	return nil, nil
+}
+
+// EmitExample returns the bytes corresponding to an example complex tree
+func (store *Store) EmitExample() []byte {
+	bytes, err := store.EmitPlainFile(stores.ExampleComplexTree.Branches)
+	if err != nil {
+		panic(err)
+	}
+	return bytes
 }
--- stores/toml/store_test.go
+++ stores/toml/store_test.go
@@ -7,29 +7,57 @@ import (
 	"go.mozilla.org/sops/v3"
 )
 
-var PLAIN = []byte(`---
-# comment 0
-key1: value
-key1_a: value
-# ^ comment 1
----
-key2: value2`)
+// Example form https://en.wikipedia.org/wiki/TOML
+var PLAIN = []byte(`
+# This is a TOML document.
 
-var BRANCHES = sops.TreeBranches{
-	sops.TreeBranch{
-		sops.TreeItem{
-			Key:   "key1",
-			Value: "value",
-		},
-		sops.TreeItem{
-			Key:   "key1_a",
-			Value: "value",
-		},
-		sops.TreeItem{
-			Key:   sops.Comment{" ^ comment 1"},
-			Value: nil,
-		},
+title = "TOML Example"
+
+[owner]
+name = "Tom Preston-Werner"
+dob = 1979-05-27T07:32:00-08:00 # First class dates
+
+[database]
+server = "192.168.1.1"
+ports = [ 8001, 8001, 8002 ]
+connection_max = 5000
+enabled = true
+
+[servers]
+
+  # Indentation (tabs and/or spaces) is allowed but not required
+  [servers.alpha]
+  ip = "10.0.0.1"
+  dc = "eqdc10"
+
+  [servers.beta]
+  ip = "10.0.0.2"
+  dc = "eqdc10"
+
+[clients]
+data = [ ["gamma", "delta"], [1, 2] ]
+
+# Line breaks are OK when inside arrays
+hosts = [
+  "alpha",
+  "omega"
+]
+`)
+
+var BRANCHES = sops.TreeBranches{sops.TreeBranch{
+	sops.TreeItem{
+		Key:   "key1",
+		Value: "value",
 	},
+	sops.TreeItem{
+		Key:   "key1_a",
+		Value: "value",
+	},
+	sops.TreeItem{
+		Key:   sops.Comment{Value: " ^ comment 1"},
+		Value: nil,
+	},
+},
 	sops.TreeBranch{
 		sops.TreeItem{
 			Key:   "key2",
@@ -38,8 +66,111 @@ var BRANCHES = sops.TreeBranches{
 	},
 }
 
+var plainNested = []byte(`
+[glossary]
+title = "example glossary"
+
+[glossary.GlossDiv]
+title="S"
+
+[glossary.GlossDiv.GlossList.GlossEntry]
+ID = "SGML"
+SortAs = "SGML"
+GlossTerm = "Standard Generalized Markup Language"
+Acronym ="SGML"
+Abbrev ="ISO 8879:1986"
+GlossSee = "markup"
+               
+
+[glossary.GlossDiv.GlossList.GlossEntry.GlossDef] 
+para = "A meta-markup language, used to create markup languages such as DocBook."
+GlossSeeAlso =[
+	"GML", 
+	"XML"
+]
+`)
+
+var branchesNested = sops.TreeBranches{
+	sops.TreeBranch{
+		sops.TreeItem{
+			Key: "glossary",
+			Value: sops.TreeBranch{
+				sops.TreeItem{
+					Key:   "title",
+					Value: "example glossary",
+				},
+				sops.TreeItem{
+					Key: "GlossDiv",
+					Value: sops.TreeBranch{
+						sops.TreeItem{
+							Key:   "title",
+							Value: "S",
+						},
+						sops.TreeItem{
+							Key: "GlossList",
+							Value: sops.TreeBranch{
+								sops.TreeItem{
+									Key: "GlossEntry",
+									Value: sops.TreeBranch{
+										sops.TreeItem{
+											Key:   "ID",
+											Value: "SGML",
+										},
+										sops.TreeItem{
+											Key:   "SortAs",
+											Value: "SGML",
+										},
+										sops.TreeItem{
+											Key:   "GlossTerm",
+											Value: "Standard Generalized Markup Language",
+										},
+										sops.TreeItem{
+											Key:   "Acronym",
+											Value: "SGML",
+										},
+										sops.TreeItem{
+											Key:   "Abbrev",
+											Value: "ISO 8879:1986",
+										},
+										sops.TreeItem{
+											Key:   "GlossSee",
+											Value: "markup",
+										},
+										sops.TreeItem{
+											Key: "GlossDef",
+											Value: sops.TreeBranch{
+												sops.TreeItem{
+													Key:   "para",
+													Value: "A meta-markup language, used to create markup languages such as DocBook.",
+												},
+												sops.TreeItem{
+													Key: "GlossSeeAlso",
+													Value: []interface{}{
+														"GML",
+														"XML",
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+}
+
 func TestLoadPlainFile(t *testing.T) {
 	branches, err := (&Store{}).LoadPlainFile(PLAIN)
 	assert.Nil(t, err)
 	assert.Equal(t, BRANCHES, branches)
+}
+
+func TestLoadPlainCompledFile(t *testing.T) {
+	branches, err := (&Store{}).LoadPlainFile(plainNested)
+	assert.Nil(t, err)
+	assert.Equal(t, branchesNested, branches)
 }
