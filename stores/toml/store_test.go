3d519f4b06128bc6215da3e6c59ded47f1efa5ab
Matthieu Dartiguenave <matthieu.dartiguenave@gmail.com>
2020-07-26 09:34:26 +0100

    Prepare toml tests

---
+++ stores/toml/store_test.go
@@ -0,0 +1,45 @@
+package toml
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.mozilla.org/sops/v3"
+)
+
+var PLAIN = []byte(`---
+# comment 0
+key1: value
+key1_a: value
+# ^ comment 1
+---
+key2: value2`)
+
+var BRANCHES = sops.TreeBranches{
+	sops.TreeBranch{
+		sops.TreeItem{
+			Key:   "key1",
+			Value: "value",
+		},
+		sops.TreeItem{
+			Key:   "key1_a",
+			Value: "value",
+		},
+		sops.TreeItem{
+			Key:   sops.Comment{" ^ comment 1"},
+			Value: nil,
+		},
+	},
+	sops.TreeBranch{
+		sops.TreeItem{
+			Key:   "key2",
+			Value: "value2",
+		},
+	},
+}
+
+func TestLoadPlainFile(t *testing.T) {
+	branches, err := (&Store{}).LoadPlainFile(PLAIN)
+	assert.Nil(t, err)
+	assert.Equal(t, BRANCHES, branches)
+}
